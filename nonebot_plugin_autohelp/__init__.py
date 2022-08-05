472b34aa05e0495ebeeeb352c1edc877176481b0
freemt <yucongo+fmt@gmail.com>
2022-08-05 11:45:12 +0800

    Bump version from 0.1.4 to 0.1.5, description fix

--- nonebot_plugin_autohelp/__init__.py
+++ nonebot_plugin_autohelp/__init__.py
@@ -3,7 +3,7 @@
 For more info on usage:
 /help -h
 """
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 from .nonebot_plugin_autohelp import nonebot_plugin_autohelp
 
 __all__ = ("nonebot_plugin_autohelp",)
