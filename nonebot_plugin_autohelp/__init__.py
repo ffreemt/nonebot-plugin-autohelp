fd3da43dfa32e4e387af4890508fc431c15a3d15
freemt <yucongo+fmt@gmail.com>
2022-08-05 12:31:44 +0800

    Bump to 0.1.6, __init__.py utf8 fix with sync-version fix

--- nonebot_plugin_autohelp/__init__.py
+++ nonebot_plugin_autohelp/__init__.py
@@ -1,9 +1,9 @@
-"""Autogenerate response to commands /help !help help ≤Àµ• caidan /i.
+r"""Autogenerate response to commands /help !help help ËèúÂçï caidan /i.
 
 For more info on usage:
 /help -h
 """
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 from .nonebot_plugin_autohelp import nonebot_plugin_autohelp
 
 __all__ = ("nonebot_plugin_autohelp",)
