60ecd7ff6dd25486a2a6e44437ebfa1af8b99451
ffreemt <yucongo+fmt@gmail.com>
2022-10-16 00:38:33 +0800

    Update from 0.1.6 to 0.1.7, fix State() due to nb rc1 change

--- nonebot_plugin_autohelp/__init__.py
+++ nonebot_plugin_autohelp/__init__.py
@@ -3,7 +3,7 @@ r"""Autogenerate response to commands /help !help help 菜单 caidan /i.
 For more info on usage:
 /help -h
 """
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 from .nonebot_plugin_autohelp import nonebot_plugin_autohelp
 
 __all__ = ("nonebot_plugin_autohelp",)
